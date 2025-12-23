6a6c8e0e73f88fa89684467e617575d3a46ae12d
MJ-1112 <m22327803@gmail.com>
2025-12-23 15:04:04 +0530

    cors err fix 2

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,14 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-    origin:[ "http://localhost:5173",'https://sqac-member-form.vercel.app/'],
-    methods:['GET','POST','PUT','DELETE'],
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://sqac-member-form.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 
 function slugify(text) {
   return text
